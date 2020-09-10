d8d6e1c32e32e19a977938ada6bb904e604a15f6
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-10 16:49:59 +0700

    update: typo config

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -26,7 +26,87 @@ const theme = createMuiTheme({
       "Helvetica Neue",
       "sans-serif"
     ].join(","),
-    fontSize: 13
+    fontSize: 14,
+    h1: {
+      fontWeight: 300,
+      fontSize: "6rem",
+      lineHeight: 1.167,
+      letterSpacing: "-0.01562em"
+    },
+    h2: {
+      fontWeight: 300,
+      fontSize: "3.75rem",
+      lineHeight: 1.2,
+      letterSpacing: "-0.00833em"
+    },
+    h3: {
+      fontWeight: 400,
+      fontSize: "3rem",
+      lineHeight: 1.167,
+      letterSpacing: "0em"
+    },
+    h4: {
+      fontWeight: 400,
+      fontSize: "2.125rem",
+      lineHeight: 1.235,
+      letterSpacing: "0.00735em"
+    },
+    h5: {
+      fontWeight: 400,
+      fontSize: "1.5rem",
+      lineHeight: 1.334,
+      letterSpacing: "0em"
+    },
+    h6: {
+      fontWeight: 500,
+      fontSize: "1.25rem",
+      lineHeight: 1.6,
+      letterSpacing: "0.0075em"
+    },
+    subtitle1: {
+      fontWeight: 400,
+      fontSize: "1rem",
+      lineHeight: 1.75,
+      letterSpacing: "0.00938em"
+    },
+    subtitle2: {
+      fontWeight: 500,
+      fontSize: "0.875rem",
+      lineHeight: 1.57,
+      letterSpacing: "0.00714em"
+    },
+    body1: {
+      fontWeight: 400,
+      fontSize: "1rem",
+      lineHeight: 1.5,
+      letterSpacing: "0.00938em"
+    },
+    body2: {
+      fontWeight: 400,
+      fontSize: "0.875rem",
+      lineHeight: 1.43,
+      letterSpacing: "0.01071em"
+    },
+    button: {
+      fontWeight: 500,
+      fontSize: "0.875rem",
+      lineHeight: 1.75,
+      letterSpacing: "0.02857em",
+      textTransform: "uppercase"
+    },
+    caption: {
+      fontWeight: 400,
+      fontSize: "0.75rem",
+      lineHeight: 1.66,
+      letterSpacing: "0.03333em"
+    },
+    overline: {
+      fontWeight: 400,
+      fontSize: "0.75rem",
+      lineHeight: 2.66,
+      letterSpacing: "0.08333em",
+      textTransform: "uppercase"
+    }
   },
   breakpoints: {
     values: {
