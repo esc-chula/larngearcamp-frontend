62a87723ac77589fa2ecf48d103a3422f588ffd9
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-10 20:49:17 +0700

    change: breakpoint numbers

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -111,10 +111,10 @@ const theme = createMuiTheme({
   breakpoints: {
     values: {
       xs: 0,
-      sm: 600,
-      md: 960,
-      lg: 1280,
-      xl: 1920
+      sm: 480,
+      md: 640,
+      lg: 840,
+      xl: 1280
     }
   },
   overrides: {
