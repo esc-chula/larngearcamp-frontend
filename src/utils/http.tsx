c478de66745453db950eca433ed1cbae38f91a11
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-11 19:07:33 +0700

    create: http utils

---
+++ src/utils/http.tsx
@@ -0,0 +1,34 @@
+import axios from "axios"
+import { AxiosRequestConfig } from "axios"
+import { getLocalStorage } from "./storage"
+
+const httpClient = axios.create({
+  baseURL: process.env.REACT_APP_API_SERVER
+})
+
+const authHeaderConfig = (config: AxiosRequestConfig) => {
+  const accessToken = getLocalStorage("ACCESS_TOKEN")
+  config.headers.Authorization = accessToken ? `Bearer ${accessToken}` : ""
+  console.log(config)
+  return config
+}
+
+httpClient.interceptors.request.use(authHeaderConfig)
+
+async function get<T>(url: string, params?: any): Promise<T> {
+  return await httpClient.get(url, params)
+}
+
+async function post<D, T>(url: string, data?: D): Promise<T> {
+  return await httpClient.post(url, data)
+}
+
+async function put<D, T>(url: string, data?: D): Promise<T> {
+  return await httpClient.put(url, data)
+}
+
+async function del(url: string): Promise<void> {
+  await httpClient.delete(url)
+}
+
+export { httpClient, get, post, put, del }
