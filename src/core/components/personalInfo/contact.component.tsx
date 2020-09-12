3a32fcdb80f89ae26da722e17bb3a74e3bb8268d
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-12 16:04:42 +0700

    create: second application step partition

---
+++ src/core/components/personalInfo/contact.component.tsx
@@ -0,0 +1,89 @@
+import React, { useCallback } from "react"
+import { useFormContext } from "react-hook-form"
+import { Typography, Box, Grid, TextField } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => ({
+  bold: {
+    fontWeight: "bold"
+  },
+  title: {
+    marginBottom: theme.spacing(2)
+  }
+}))
+
+const PersonalContactComponent = () => {
+  const { register, setValue } = useFormContext()
+  const classes = useStyles()
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target.name, event.target.value)
+    },
+    [setValue]
+  )
+  return (
+    <>
+      <Box mt={2}>
+        <Typography variant="h6" className={`${classes.bold} ${classes.title}`}>
+          ข้อมูลส่วนตัว
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={2}>
+            <TextField
+              id="title"
+              name="title"
+              label="คำนำหน้า"
+              variant="outlined"
+              type="text"
+              onChange={handleChange}
+              ref={register}
+              size="small"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={5}>
+            <TextField
+              id="name"
+              name="name"
+              label="ชื่อจริง"
+              variant="outlined"
+              type="text"
+              onChange={handleChange}
+              ref={register}
+              size="small"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={5}>
+            <TextField
+              id="surname"
+              name="surname"
+              label="นามสกุล"
+              variant="outlined"
+              type="text"
+              onChange={handleChange}
+              ref={register}
+              size="small"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <TextField
+              id="nickname"
+              name="nickname"
+              label="ชื่อเล่น"
+              variant="outlined"
+              type="text"
+              onChange={handleChange}
+              ref={register}
+              size="small"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  )
+}
+
+export { PersonalContactComponent }
