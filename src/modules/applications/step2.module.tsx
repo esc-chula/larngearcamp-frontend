853525fa76984515fc87c8953d5245c027987009
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-12 16:05:06 +0700

    add: second step partition

--- src/modules/applications/step2.module.tsx
+++ src/modules/applications/step2.module.tsx
@@ -1,7 +1,85 @@
-import React from "react"
+import React, { useCallback } from "react"
+import { CardComponent } from "../../core/components/card.component"
+import { Button, Grid } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import { useHistory } from "react-router-dom"
+import { useGlobalContext } from "../../core/providers/global.provider"
+import { FormProvider, useForm } from "react-hook-form"
+import { PersonalProfileComponent } from "../../core/components/personalInfo/profile.component"
+import { PersonalContactComponent } from "../../core/components/personalInfo/contact.component"
+import { PersonalEducationComponent } from "../../core/components/personalInfo/education.component"
+import { PersonalHealthComponent } from "../../core/components/personalInfo/health.component"
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    marginTop: theme.spacing(4)
+  },
+  divider: {
+    marginBottom: theme.spacing(2)
+  },
+  bold: {
+    fontWeight: "bold"
+  },
+  buttonSuccess: {
+    marginTop: theme.spacing(2),
+    color: "white",
+    background: theme.palette.success.main,
+    "&:hover": {
+      background: theme.palette.success.dark
+    }
+  },
+  buttonWarning: {
+    marginTop: theme.spacing(2),
+    color: "white",
+    background: theme.palette.warning.main,
+    "&:hover": {
+      background: theme.palette.warning.dark
+    }
+  }
+}))
 
 const ApplicationStepTwoModule = () => {
-  return <></>
+  const classes = useStyles()
+  const history = useHistory()
+  const methods = useForm()
+  const { setStep } = useGlobalContext()
+  const { handleSubmit } = methods
+  const nextPage = useCallback(
+    (path: string) => () => {
+      setStep(2)
+      history.push(path)
+    },
+    [history, setStep]
+  )
+
+  const onSubmit = useCallback(() => {
+    console.log("Success")
+  }, [])
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <CardComponent maxWidth="lg" className={classes.card}>
+          <PersonalProfileComponent />
+          <PersonalEducationComponent />
+          <PersonalHealthComponent />
+          <PersonalContactComponent />
+          <Grid container spacing={2}>
+            <Grid xs={6} item>
+              <Button onClick={nextPage("/application/step1")} variant="contained" className={classes.buttonWarning} fullWidth>
+                ไปขั้นตอนถัดไป
+              </Button>
+            </Grid>
+            <Grid xs={6} item>
+              <Button onClick={nextPage("/application/step3")} variant="contained" className={classes.buttonSuccess} fullWidth>
+                ไปขั้นตอนถัดไป
+              </Button>
+            </Grid>
+          </Grid>
+        </CardComponent>
+      </form>
+    </FormProvider>
+  )
 }
 
 export default ApplicationStepTwoModule
