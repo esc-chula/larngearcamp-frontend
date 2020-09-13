f0399f282184119837096768be9f26b278be7d0c
thanadolps <thanadolps@gmail.com>
2020-09-14 01:20:55 +0700

    add: import react-multi-carousel's css in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./styles/index.scss"
+import "react-multi-carousel/lib/styles.css"
 
 import { AppModule } from "./modules/app.module"
 
