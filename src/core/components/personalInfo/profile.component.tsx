0569343465fb83f4b37b94b7575311eef0f2d071
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-13 14:35:11 +0700

    add: more input field

--- src/core/components/personalInfo/profile.component.tsx
+++ src/core/components/personalInfo/profile.component.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useState } from "react"
 import { useFormContext } from "react-hook-form"
-import { Typography, Box, Grid, TextField } from "@material-ui/core"
+import { Typography, Box, Grid, TextField, MenuItem, FormControl, InputLabel, Select } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles(theme => ({
@@ -13,11 +13,14 @@ const useStyles = makeStyles(theme => ({
 }))
 
 const PersonalProfileComponent = () => {
+  const [title, setTitle] = useState("")
   const { register, setValue } = useFormContext()
   const classes = useStyles()
+
   const handleChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
+    event => {
       setValue(event.target.name, event.target.value)
+      event.target.name === "title" && setTitle(event.target.value)
     },
     [setValue]
   )
@@ -29,17 +32,13 @@ const PersonalProfileComponent = () => {
         </Typography>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6} md={2}>
-            <TextField
-              id="title"
-              name="title"
-              label="คำนำหน้า"
-              variant="outlined"
-              type="text"
-              onChange={handleChange}
-              ref={register}
-              size="small"
-              fullWidth
-            />
+            <FormControl variant="outlined" size="small" fullWidth>
+              <InputLabel id="title-label">คำนำหน้า</InputLabel>
+              <Select labelId="title-label" id="title" name="title" value={title} onChange={handleChange} label="คำนำหน้า">
+                <MenuItem value="นาย">นาย</MenuItem>
+                <MenuItem value="นางสาว">นางสาว</MenuItem>
+              </Select>
+            </FormControl>
           </Grid>
           <Grid item xs={12} sm={6} md={5}>
             <TextField
@@ -79,6 +78,46 @@ const PersonalProfileComponent = () => {
               size="small"
               fullWidth
             />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <TextField
+              id="birthDate"
+              name="birthDate"
+              variant="outlined"
+              type="date"
+              label="วันเกิด"
+              onChange={handleChange}
+              ref={register}
+              size="small"
+              fullWidth
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <TextField
+              id="age"
+              name="age"
+              label="อายุ"
+              variant="outlined"
+              type="text"
+              onChange={handleChange}
+              ref={register}
+              size="small"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <FormControl variant="outlined" size="small" fullWidth>
+              <InputLabel id="religion-label">ศาสนา</InputLabel>
+              <Select labelId="religion-label" id="religion" name="religion" value={title} onChange={handleChange} label="ศาสนา">
+                <MenuItem value="พุทธ">พุทธ</MenuItem>
+                <MenuItem value="คริสต์">คริสต์</MenuItem>
+                <MenuItem value="อิสลาม">อิสลาม</MenuItem>
+                <MenuItem value="พราหมณ์-ฮินดู">พราหมณ์-ฮินดู</MenuItem>
+              </Select>
+            </FormControl>
           </Grid>
         </Grid>
       </Box>
