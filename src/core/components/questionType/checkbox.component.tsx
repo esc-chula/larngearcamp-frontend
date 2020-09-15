d294e9284fda96b40c28c4ea77bf82a201a9aca5
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-15 22:14:24 +0700

    create: checkbox type input for step 3 and 4

--- src/core/components/questionType/checkbox.component.tsx
+++ src/core/components/questionType/checkbox.component.tsx
@@ -1,1 +1,35 @@
-export {}
+import React from "react"
+import { Controller, useFormContext } from "react-hook-form"
+import { Checkbox, FormControlLabel, Box } from "@material-ui/core"
+import ChoiceModel from "../../models/choice.model"
+
+interface CheckboxTypeProps {
+  name: string
+  contents: Array<ChoiceModel> | undefined
+  className?: any
+}
+
+const CheckboxTypeComponent: React.FC<CheckboxTypeProps> = ({ name, contents, ...other }) => {
+  const { control } = useFormContext()
+  return (
+    <Box mt={3} display="flex" flexDirection="column">
+      {contents?.map((content, index) => (
+        <Controller
+          key={index}
+          name={`choice-${index}`}
+          control={control}
+          defaultValue={false}
+          {...other}
+          render={({ onChange, onBlur, value, name }) => (
+            <FormControlLabel
+              label={content.label}
+              control={<Checkbox color="primary" onBlur={onBlur} onChange={e => onChange(e.target.checked)} checked={value} />}
+            />
+          )}
+        />
+      ))}
+    </Box>
+  )
+}
+
+export { CheckboxTypeComponent }
