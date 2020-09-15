7ae7a99f2a08beb945f40d7aafdaf98f18b54023
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-15 22:13:47 +0700

    create: multiline type input for step3

--- src/core/components/questionType/multiline.component.tsx
+++ src/core/components/questionType/multiline.component.tsx
@@ -1,1 +1,23 @@
-export {}
+import React from "react"
+import { StandardTextFieldProps, TextField } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import { useFormContext } from "react-hook-form"
+
+const useStyles = makeStyles(theme => ({
+  mutiline: {
+    marginTop: theme.spacing(3),
+    "&>*": {
+      minHeight: 80,
+      display: "flex",
+      alignItems: "flex-start"
+    }
+  }
+}))
+
+const MultilineTypeComponent: React.FC<StandardTextFieldProps> = ({ ...other }) => {
+  const classes = useStyles()
+  const { register } = useFormContext()
+  return <TextField variant="outlined" size="small" multiline ref={register} {...other} className={classes.mutiline} />
+}
+
+export { MultilineTypeComponent }
