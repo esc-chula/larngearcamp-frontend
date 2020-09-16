203ca4d18581b17fa2af54cdd17320d8d9925067
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-16 13:45:53 +0700

    prepare: for admin page

---
+++ src/modules/admin/dashboard.module.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Typography } from "@material-ui/core"
+
+const AdminDashboardModule = () => {
+  return <Typography variant="h1">Admin Dashboard ver 1.0.0 </Typography>
+}
+
+export { AdminDashboardModule }
