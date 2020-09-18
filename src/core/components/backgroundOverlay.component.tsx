549a065a236a6c268f7451add9fe5a1f2f89a113
thanadolps <thanadolps@gmail.com>
2020-09-19 00:55:34 +0700

    comment & format background overlay

--- src/core/components/backgroundOverlay.component.tsx
+++ src/core/components/backgroundOverlay.component.tsx
@@ -2,47 +2,55 @@ import React from "react"
 import { makeStyles, Theme } from "@material-ui/core/styles"
 
 type Props = {
-    src: string,
-    aspectRatio: number,
-    contentPercentage?: number,
-    disableAutoColor?: boolean
+  src: string
+  aspectRatio: number
+  contentPercentage?: number
+  disableAutoColor?: boolean
 }
 
 const useStyle = makeStyles<Theme, Props>(theme => ({
-    backgroundImg: {
-        zIndex: -1,
-        width: '100%',
-        position: 'absolute'
-    },
-    contentContainer: {
-        width:"100%",
-        height: 0,
-        paddingTop: props => `${(props.contentPercentage || 100)/props.aspectRatio}%`,
-        position: "relative"
-    },
-    content: {
-        position: "absolute",
-        top: 0,
-        left:0,
-        width: '100%',
-        height: '100%',
-        color: props => props.disableAutoColor ? '' : theme.palette.primary.contrastText
-    }
+  backgroundImg: {
+    zIndex: -1,
+    width: "100%",
+    position: "absolute"
+  },
+  contentContainer: {
+    width: "100%",
+    height: 0,
+    paddingTop: props => `${(props.contentPercentage || 100) / props.aspectRatio}%`,
+    position: "relative"
+  },
+  content: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    color: props => (props.disableAutoColor ? "" : theme.palette.primary.contrastText)
+  }
 }))
 
-const BackgroundOverlay: React.FC<Props> = (props) => {
-    const classes = useStyle(props);
+/**
+ * Component, Image with content (child elements) overlayed on top
+ *
+ * src: Image source, like in <img>
+ *
+ * aspectRatio: Image aspect ratio, use for content alignment
+ *
+ * contentPercentage: percentage of content height will be (with respected to image height),
+ * note that other element are only effected by content height
+ */
+const BackgroundOverlay: React.FC<Props> = props => {
+  const classes = useStyle(props)
 
-    return (
+  return (
     <div>
-        <img src={props.src} className={classes.backgroundImg}/>
-        <div className={classes.contentContainer}>
-            <div className={classes.content}>
-                {props.children}
-            </div>
-        </div>
+      <img src={props.src} className={classes.backgroundImg} />
+      <div className={classes.contentContainer}>
+        <div className={classes.content}>{props.children}</div>
+      </div>
     </div>
-    )
+  )
 }
 
 export default BackgroundOverlay
