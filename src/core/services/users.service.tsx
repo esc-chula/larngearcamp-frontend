8ff4f326047a4f7714dcb0b8e29d63e74e625151
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-19 17:37:45 +0700

    fix: exp problem

--- src/core/services/users.service.tsx
+++ src/core/services/users.service.tsx
@@ -5,31 +5,31 @@ import { httpClient } from "../../utils/http"
 
 const getUsers = async (): Promise<AxiosResponse> => {
   try {
-    process.env.NODE_ENV === "development" && console.log("on request: getUsers")
+    process.env.REACT_APP_DEBUG && console.log("on request: getUsers")
     const result = await httpClient.get(`${process.env.REACT_APP_API_SERVER}/users`)
-    process.env.NODE_ENV === "development" && console.log(result)
+    process.env.REACT_APP_DEBUG && console.log(result)
     return result
   } catch (error) {
-    process.env.NODE_ENV === "development" && console.log(error.response)
+    process.env.REACT_APP_DEBUG && console.log(error.response)
     return error.response
   }
 }
 
 const getUser = async (params: { userId: string } | undefined): Promise<AxiosResponse> => {
   try {
-    process.env.NODE_ENV === "development" && console.log("on request: getUser")
+    process.env.REACT_APP_DEBUG && console.log("on request: getUser")
     const result = await httpClient.get(`${process.env.REACT_APP_API_SERVER}/users/${params?.userId}`)
-    process.env.NODE_ENV === "development" && console.log(result)
+    process.env.REACT_APP_DEBUG && console.log(result)
     return result
   } catch (error) {
-    process.env.NODE_ENV === "development" && console.log(error.response)
+    process.env.REACT_APP_DEBUG && console.log(error.response)
     return error.response
   }
 }
 
 const createUser = async (params: UserModel | undefined): Promise<AxiosResponse> => {
   try {
-    process.env.NODE_ENV === "development" && console.log("on request: createUser")
+    process.env.REACT_APP_DEBUG && console.log("on request: createUser")
     const result = await httpClient.post(`${process.env.REACT_APP_API_SERVER}/users`, {
       email: params?.email,
       password: params?.password
@@ -37,34 +37,34 @@ const createUser = async (params: UserModel | undefined): Promise<AxiosResponse>
     console.log(result)
     return result
   } catch (error) {
-    process.env.NODE_ENV === "development" && console.log(error.response)
+    process.env.REACT_APP_DEBUG && console.log(error.response)
     return error.response
   }
 }
 
 const updateUser = async (params: UserModel | undefined): Promise<AxiosResponse> => {
   try {
-    process.env.NODE_ENV === "development" && console.log("on request: updateUser")
+    process.env.REACT_APP_DEBUG && console.log("on request: updateUser")
     const result = await httpClient.patch(`${process.env.REACT_APP_API_SERVER}/users`, {
       email: params?.email,
       password: params?.password
     })
-    process.env.NODE_ENV === "development" && console.log(result)
+    process.env.REACT_APP_DEBUG && console.log(result)
     return result
   } catch (error) {
-    process.env.NODE_ENV === "development" && console.log(error.response)
+    process.env.REACT_APP_DEBUG && console.log(error.response)
     return error.response
   }
 }
 
 const deleteUser = async (params: { userId: string } | undefined): Promise<AxiosResponse> => {
   try {
-    process.env.NODE_ENV === "development" && console.log("on request: deleteUser")
+    process.env.REACT_APP_DEBUG && console.log("on request: deleteUser")
     const result = await axios.delete(`${process.env.REACT_APP_API_SERVER}/users/${params?.userId}`)
-    process.env.NODE_ENV === "development" && console.log(result)
+    process.env.REACT_APP_DEBUG && console.log(result)
     return result
   } catch (error) {
-    process.env.NODE_ENV === "development" && console.log(error.response)
+    process.env.REACT_APP_DEBUG && console.log(error.response)
     return error.response
   }
 }
