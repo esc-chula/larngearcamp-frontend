107e2978f792c539887d2064733809c1540b14be
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-21 21:18:09 +0700

    add: error message

--- src/core/components/select.component.tsx
+++ src/core/components/select.component.tsx
@@ -1,7 +1,8 @@
 import React from "react"
-import { InputLabel, Select, MenuItem, FormControlProps, FormControl } from "@material-ui/core"
+import { InputLabel, Select, MenuItem, FormControlProps, FormControl, FormHelperText } from "@material-ui/core"
 import SelectModel from "../models/select.model"
-import { Controller } from "react-hook-form"
+import { Controller, useFormContext } from "react-hook-form"
+import { resolve } from "../../utils/other"
 
 interface SelectProps extends FormControlProps {
   data: SelectModel
@@ -9,6 +10,8 @@ interface SelectProps extends FormControlProps {
 }
 
 const SelectComponent: React.FC<SelectProps> = ({ control, data }) => {
+  const { errors } = useFormContext()
+  const selfError = resolve(data.name, errors)
   return (
     <Controller
       name={data.name}
@@ -16,14 +19,15 @@ const SelectComponent: React.FC<SelectProps> = ({ control, data }) => {
       defaultValue=""
       render={props => (
         <FormControl fullWidth variant="outlined" size="small">
-          <InputLabel>{data.label}</InputLabel>
-          <Select label={data.label} {...props}>
+          <InputLabel error={!!selfError}>{data.label}</InputLabel>
+          <Select label={data.label} {...props} error={!!selfError}>
             {data.contents.map((content, index) => (
               <MenuItem value={content.value} key={data.name + content.text + content.value + index}>
                 {content.text}
               </MenuItem>
             ))}
           </Select>
+          <FormHelperText error={!!selfError}>{selfError?.message}</FormHelperText>
         </FormControl>
       )}
     />
