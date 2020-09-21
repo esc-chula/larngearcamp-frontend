90795d00a01f26fd613f3d87ca8b5353b34059ab
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-21 21:20:45 +0700

    forget to commit

--- src/core/components/personalInfo/profile.component.tsx
+++ src/core/components/personalInfo/profile.component.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import { useFormContext } from "react-hook-form"
-import { Typography, Box, Grid, TextField, TextFieldProps } from "@material-ui/core"
+import { Typography, Box, Grid } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { titlesConstant } from "../../constants/titles.constants"
 import { SelectComponent } from "../select.component"
 import { religionsConstant } from "../../constants/religions.constant"
 import { useAuthContext } from "../../providers/auth.provider"
+import { TextFieldComponent } from "../textField.component"
 
 const useStyles = makeStyles(theme => ({
   bold: {
@@ -16,14 +17,8 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-const inputPropsConstant: TextFieldProps = {
-  size: "small",
-  fullWidth: true,
-  variant: "outlined"
-}
-
 const PersonalProfileComponent = () => {
-  const { register, control } = useFormContext()
+  const { control } = useFormContext()
   const classes = useStyles()
   const { me } = useAuthContext()
 
@@ -38,27 +33,25 @@ const PersonalProfileComponent = () => {
             <SelectComponent control={control} data={titlesConstant} />
           </Grid>
           <Grid item xs={12} sm={6} md={5}>
-            <TextField name="name" label="ชื่อจริง" type="text" inputRef={register} defaultValue={me.data?.name?.first} {...inputPropsConstant} />
+            <TextFieldComponent name="name" label="ชื่อจริง" type="text" defaultValue={me.data?.name?.first} />
           </Grid>
           <Grid item xs={12} sm={6} md={5}>
-            <TextField name="surname" label="นามสกุล" type="text" inputRef={register} defaultValue={me.data?.name?.second} {...inputPropsConstant} />
+            <TextFieldComponent name="surname" label="นามสกุล" type="text" defaultValue={me.data?.name?.second} />
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <TextField name="nickname" label="ชื่อเล่น" type="text" inputRef={register} {...inputPropsConstant} />
+            <TextFieldComponent name="nickname" label="ชื่อเล่น" type="text" />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
             <SelectComponent data={religionsConstant} control={control} />
           </Grid>
           <Grid item xs={12} sm={12} md={5}>
-            <TextField
+            <TextFieldComponent
               name="birthDate"
-              type="date"
               label="วันเกิด"
-              inputRef={register}
+              type="date"
               InputLabelProps={{
                 shrink: true
               }}
-              {...inputPropsConstant}
             />
           </Grid>
         </Grid>
