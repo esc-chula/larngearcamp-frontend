71683339fd17dec79d0bf5cbb87da38785d28563
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-22 00:49:48 +0700

    change: name aloong with answer schema

--- src/core/components/questionType/checkbox.component.tsx
+++ src/core/components/questionType/checkbox.component.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Controller, useFormContext } from "react-hook-form"
 import { Checkbox, FormControlLabel, Box } from "@material-ui/core"
 import ChoiceModel from "../../models/choice.model"
+import { sequenceConstant } from "../../constants/sequence.constant"
 
 interface CheckboxTypeProps {
   name: string
@@ -13,22 +14,25 @@ const CheckboxTypeComponent: React.FC<CheckboxTypeProps> = ({ name, contents, ..
   const { control } = useFormContext()
   return (
     <Box mt={3} display="flex" flexDirection="column">
-      {contents?.map((content, index) => (
-        <Controller
-          key={index}
-          name={`choice-${index}`}
-          control={control}
-          defaultValue={false}
-          {...other}
-          render={({ onChange, onBlur, value, name }) => (
-            <FormControlLabel
-              name={name}
-              label={content.label}
-              control={<Checkbox color="primary" onBlur={onBlur} onChange={e => onChange(e.target.checked)} checked={value} />}
-            />
-          )}
-        />
-      ))}
+      {contents?.map((content, index) => {
+        const newName = `${name}.${sequenceConstant[index + 1]}`
+        return (
+          <Controller
+            key={index}
+            name={newName}
+            control={control}
+            defaultValue={false}
+            {...other}
+            render={({ onChange, onBlur, value, name }) => (
+              <FormControlLabel
+                name={name}
+                label={content.label}
+                control={<Checkbox color="primary" onBlur={onBlur} onChange={e => onChange(e.target.checked)} checked={value} />}
+              />
+            )}
+          />
+        )
+      })}
     </Box>
   )
 }
