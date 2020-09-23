e7b44c5c6872dc018267a019902511c1e7708b8c
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-23 18:25:29 +0700

    change: bloodTypes and grades constant

--- src/core/constants/bloodGroups.constant.tsx
+++ src/core/constants/bloodGroups.constant.tsx
@@ -4,13 +4,11 @@ const bloodGroupsConstant: SelectModel = {
   name: "health.bloodType",
   label: "หมู่เลือด",
   contents: [
-    { text: "A+", value: "A+" },
-    { text: "A-", value: "A+" },
-    { text: "B+", value: "B+" },
-    { text: "B-", value: "B-" },
-    { text: "AB+", value: "AB+" },
-    { text: "O+", value: "O+" },
-    { text: "O-", value: "O-" }
+    { text: "A", value: "A" },
+    { text: "B", value: "B" },
+    { text: "AB", value: "AB" },
+    { text: "O", value: "O" },
+    { text: "ไม่ทราบ", value: "ไม่ทราบ" }
   ]
 }
 
