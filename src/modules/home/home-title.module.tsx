6a2281442484d07501ef88a856d7d4fcf5090563
thanadolps <thanadolps@gmail.com>
2020-09-24 05:31:59 +0700

    fix: title's font

--- src/modules/home/home-title.module.tsx
+++ src/modules/home/home-title.module.tsx
@@ -9,12 +9,20 @@ import BackgroundOverlay from "../../core/components/backgroundOverlay.component
 
 const useStyle = makeStyles(theme => ({
   title: {
-    fontSize: pxToRem(64),
+    fontSize: pxToRem(60),
     lineHeight: pxToRem(75),
+    fontFamily: "Raleway",
     fontWeight: 300,
     margin: "0 0 20px 0"
   },
+  titleBig: {
+    fontSize: pxToRem(64)
+  },
+  titleSmall: {
+    fontSize: pxToRem(48)
+  },
   subtitle: {
+    fontSize: pxToRem(28),
     fontWeight: 200,
     lineHeight: pxToRem(42),
     margin: " 0 0 28px 0"
@@ -34,20 +42,25 @@ const HomeTitle: React.FC<BoxProps> = props => {
   const classes = useStyle()
   return (
     <BackgroundOverlay src={landing1} aspectRatio={1519 / 832} contentPercentage={70} minHeightPx={800}>
-      <Box textAlign="center" {...props}>
-        <h2 className={classes.title}>LARNGEAR CAMP 20th</h2>
-        <Typography variant="subtitle1" className={classes.subtitle}>
-          ค้นหาความเป็นวิศวกร
-          <Hidden smUp>
-            <br />
-          </Hidden>{" "}
-          ด้วยมือของคุณเอง
-        </Typography>
-        <Link to="/application" className="no-underline">
-          <Button variant="contained" color="primary" className={classes.button}>
-            <Typography className={classes.buttonText}>Apply Now</Typography>
-          </Button>
-        </Link>
+      <Box display="flex" justifyContent="center" alignItems="center" height="80%">
+        <Box textAlign="center" height="min-content" {...props}>
+          <h2 className={classes.title}>
+            <span className={classes.titleBig}>L</span>
+            <span className={classes.titleSmall}>ARNGEAR CAMP</span> <span>20th</span>
+          </h2>
+          <Typography variant="subtitle1" className={classes.subtitle}>
+            ค้นหาความเป็นวิศวกร
+            <Hidden smUp>
+              <br />
+            </Hidden>{" "}
+            ด้วยมือของคุณเอง
+          </Typography>
+          <Link to="/application" className="no-underline">
+            <Button variant="contained" color="primary" className={classes.button}>
+              <Typography className={classes.buttonText}>Apply Now</Typography>
+            </Button>
+          </Link>
+        </Box>
       </Box>
     </BackgroundOverlay>
   )
