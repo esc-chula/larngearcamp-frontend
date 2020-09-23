e62ddcbe5a8cbf091acda49c20c22b1e5681e741
thanadolps <thanadolps@gmail.com>
2020-09-24 03:03:46 +0700

    Revert "fix: font size and weight in theme.tsx"
    This reverts commit bc1309c9d8e54c2c352bf30da226eeb23542d0fb.

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -30,7 +30,7 @@ const theme = createMuiTheme({
     ].join(","),
     fontSize: 14,
     h1: {
-      fontWeight: 500,
+      fontWeight: 300,
       fontSize: "6rem",
       lineHeight: 1.167,
       letterSpacing: "-0.01562em"
@@ -42,50 +42,50 @@ const theme = createMuiTheme({
       letterSpacing: "-0.00833em"
     },
     h3: {
-      fontWeight: 300,
+      fontWeight: 400,
       fontSize: "3rem",
       lineHeight: 1.167,
       letterSpacing: "0em"
     },
     h4: {
-      fontWeight: 500,
-      fontSize: "2.5rem",
+      fontWeight: 400,
+      fontSize: "2.125rem",
       lineHeight: 1.235,
       letterSpacing: "0.00735em"
     },
     h5: {
-      fontWeight: 500,
-      fontSize: "2.25rem",
+      fontWeight: 400,
+      fontSize: "1.5rem",
       lineHeight: 1.334,
       letterSpacing: "0em"
     },
     h6: {
       fontWeight: 500,
-      fontSize: "2rem",
+      fontSize: "1.25rem",
       lineHeight: 1.6,
       letterSpacing: "0.0075em"
     },
     subtitle1: {
       fontWeight: 400,
-      fontSize: "1.75rem",
+      fontSize: "1rem",
       lineHeight: 1.75,
       letterSpacing: "0.00938em"
     },
     subtitle2: {
       fontWeight: 500,
-      fontSize: "1.5rem",
+      fontSize: "0.875rem",
       lineHeight: 1.57,
       letterSpacing: "0.00714em"
     },
     body1: {
       fontWeight: 400,
-      fontSize: "1.75rem",
+      fontSize: "1rem",
       lineHeight: 1.5,
       letterSpacing: "0.00938em"
     },
     body2: {
-      fontWeight: 300,
-      fontSize: "1.5rem",
+      fontWeight: 400,
+      fontSize: "0.875rem",
       lineHeight: 1.43,
       letterSpacing: "0.01071em"
     },
@@ -97,14 +97,14 @@ const theme = createMuiTheme({
       textTransform: "uppercase"
     },
     caption: {
-      fontWeight: 300,
-      fontSize: "1.25rem",
+      fontWeight: 400,
+      fontSize: "0.75rem",
       lineHeight: 1.66,
       letterSpacing: "0.03333em"
     },
     overline: {
-      fontWeight: 300,
-      fontSize: "1.125rem",
+      fontWeight: 400,
+      fontSize: "0.75rem",
       lineHeight: 2.66,
       letterSpacing: "0.08333em",
       textTransform: "uppercase"
