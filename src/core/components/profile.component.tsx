4727896f9fd607d54c44f70b5986377a8c15efb1
Nut Pinyo <bomb.np@gmail.com>
2020-09-24 22:44:37 +0700

    add: profile name

--- src/core/components/profile.component.tsx
+++ src/core/components/profile.component.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
+import { useAuthContext } from "../providers/auth.provider"
+import MeDTO from "../models/dto/me.dto"
+import { Typography } from "@material-ui/core"
 
 const useStyles = makeStyles(theme => ({
   profile: {
@@ -9,14 +12,26 @@ const useStyles = makeStyles(theme => ({
     background: "#181818",
     borderRadius: "100%",
     marginTop: theme.spacing(4)
+  },
+  name: {
+    marginTop: theme.spacing(2)
   }
 }))
 
 const ProfileComponent = () => {
   const classes = useStyles()
+  const { me } = useAuthContext()
+  const {
+    name: { first, last }
+  } = me.data as MeDTO
+
+  const fullName = `${first} ${last}`
   return (
     <>
       <div className={classes.profile}></div>
+      <Typography variant="h5" align="center" className={classes.name}>
+        {fullName}
+      </Typography>
     </>
   )
 }
