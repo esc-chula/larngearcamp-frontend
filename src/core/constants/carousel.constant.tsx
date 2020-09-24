f166a1aff3f8da2a0252fde53bfd2152b744bf4c
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-24 22:15:25 +0700

    move: value to constant directory

---
+++ src/core/constants/carousel.constant.tsx
@@ -0,0 +1,13 @@
+const carouselConstant = [
+  {
+    src: require("../../assets/images/carousel/1.png")
+  },
+  {
+    src: require("../../assets/images/carousel/2.jpg")
+  },
+  {
+    src: require("../../assets/images/carousel/3.jpg")
+  }
+]
+
+export default carouselConstant
---
+++ src/core/constants/contact.constant.tsx
@@ -0,0 +1,8 @@
+const contactConstant = [
+  { name: "พี่ไนซ์", tel: "086-172-3955" },
+  { name: "พี่จิน", tel: "080-795-3954" },
+  { name: "พี่น้ำว้า", tel: "084-903-4486" },
+  { name: "พี่มายดี", tel: "084-899-5449" }
+]
+
+export default contactConstant
