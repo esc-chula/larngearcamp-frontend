00665c18b6b3a115fae9595af9f55e0571989b94
Nut Pinyo <bomb.np@gmail.com>
2020-09-25 01:08:56 +0700

    add: profile status resolver

--- src/modules/profile.module.tsx
+++ src/modules/profile.module.tsx
@@ -1,10 +1,14 @@
 import React, { useCallback } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import { ProfileComponent } from "../core/components/profile.component"
-import { Container, Paper, Typography, Button } from "@material-ui/core"
+import { Container, Button } from "@material-ui/core"
 import { Link } from "react-router-dom"
 import { useApplicationContext } from "../core/providers/application.provider"
 import { useLoadingCallback } from "../core/components/loading.component"
+import { StatusInfo } from "../core/components/statusInfo.component"
+import { useAuthContext } from "../core/providers/auth.provider"
+import MeDTO from "../core/models/dto/me.dto"
+import { ProfileStatus } from "../core/models/statusInfo.model"
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -27,9 +31,44 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+const resolveStatus = (application: MeDTO["application"]): ProfileStatus => {
+  if (!application) {
+    return "start"
+  }
+
+  const { editingState, documentState, applicationState } = application
+
+  if (editingState === "FULL") {
+    return "continue"
+  }
+  if (applicationState === "PASSED_INTERVIEW") {
+    return "passedInterview"
+  }
+  if (applicationState === "NOT_PASSED_INTERVIEW") {
+    return "failedInterview"
+  }
+  if (applicationState === "PASSED_FINAL") {
+    return "passedFinal"
+  }
+  if (applicationState === "NOT_PASSED_FINAL") {
+    return "failedFinal"
+  }
+  if (documentState === "REVIEW") {
+    return "pending"
+  }
+  if (documentState === "PASSED") {
+    return "docOk"
+  }
+  return "docNotOk"
+}
+
 const ProfileModule = () => {
   const classes = useStyles()
   const { createApplication } = useApplicationContext()
+  const { me } = useAuthContext()
+  const { application } = me.data as MeDTO
+
+  const profileStatus = resolveStatus(application)
 
   const initApplication = useLoadingCallback(
     useCallback(async () => {
@@ -51,13 +90,7 @@ const ProfileModule = () => {
       <Button className={`${classes.button} ${classes.warning}`} variant="contained" fullWidth>
         แก้ไขข้อมูลส่วนตัวผู้สมัคร (ส่วนที่ 2)
       </Button>
-      <Paper elevation={2} className={classes.paper}>
-        <Typography variant="h5">รอการตรวจสอบเอกสาร</Typography>
-        <Typography variant="body2">
-          ขณะนี้อยู่ในขั้นตอนการรอตรวจสอบเอกสารที่น้อง ๆ ได้แนบเข้ามาอยู่ ขอให้น้องเข้ามาเช็ค
-          สถานะของเอกสารที่เว็บนี้เป็นประจำเผื่อในกรณีที่เอกสารที่น้องแนบมาไม่ผ่าน
-        </Typography>
-      </Paper>
+      <StatusInfo profileStatus={profileStatus} />
     </Container>
   )
 }
