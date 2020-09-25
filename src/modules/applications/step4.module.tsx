b0b7fdec62ee4291421a7e0f0195197b65b10366
Nut Pinyo <bomb.np@gmail.com>
2020-09-25 18:14:36 +0700

    fix: remove debug

--- src/modules/applications/step4.module.tsx
+++ src/modules/applications/step4.module.tsx
@@ -54,7 +54,6 @@ const ApplicationStepFourModule: React.FC = () => {
     },
     [updateApplication]
   )
-  const { getValues, errors } = methods
   const handleSubmit = useHandleSubmit(methods, onSubmit)
   return (
     <ApplicationStepModule beforeNavigate={handleSubmit}>
