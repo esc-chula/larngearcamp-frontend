b591a31d3ab70adb7072887551e106fa05fb2ee8
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-09-25 14:08:38 +0700

    fix: background component

--- src/core/components/background.component.tsx
+++ src/core/components/background.component.tsx
@@ -8,20 +8,23 @@ interface BackgroundProps {
 const useStyles = makeStyles(theme => ({
   bg: {
     width: "100%",
-    height: "100vh",
+    height: "100%",
+    overflow: "hidden",
     position: "fixed",
-    backgroundSize: "100% 100%",
+    backgroundSize: "cover",
     backgroundPosition: "center center",
     backgroundRepeat: "no-repeat",
-    //backgroundImage: (type: BackgroundProps["type"]) => `url(${require(`../../assets/images/background/${type}.svg`)})`,
-    backgroundImage: `url(${require("../../assets/images/background/bg1.svg")})`,
     zIndex: -1
-  }
+  },
+  bg1: { backgroundImage: `url(${require("../../assets/images/background/bg1.svg")})` },
+  bg2: { backgroundImage: `url(${require("../../assets/images/background/bg2.svg")})` },
+  bg3: { backgroundImage: `url(${require("../../assets/images/background/bg3.svg")})` },
+  bg4: { backgroundImage: `url(${require("../../assets/images/background/bg4.svg")})` }
 }))
 
 const BackgroundComponent: React.FC<BackgroundProps> = ({ type }) => {
   const classes = useStyles(type)
-  return <div className={classes.bg}></div>
+  return <div className={`${classes.bg} ${classes[type]}`}></div>
 }
 
 export default BackgroundComponent
