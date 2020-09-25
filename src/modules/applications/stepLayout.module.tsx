29f958f5f8a89e5d8c6bcbe64f04ba7084b68a83
paphonb <paphonb@gmail.com>
2020-09-25 17:31:58 +0700

    refactor: use react-hook-form's handleSubmit instead of custom function

--- src/modules/applications/stepLayout.module.tsx
+++ src/modules/applications/stepLayout.module.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react"
+import React, { useCallback } from "react"
 import { Container } from "@material-ui/core"
 import ButtonBar, { ButtonBarProps } from "../../core/components/buttonBar.component"
 import { FieldValues, UseFormMethods, UnpackNestedValue } from "react-hook-form"
@@ -11,28 +11,21 @@ const ApplicationStepModule: React.FC<ApplicationStepModuleProps> = ({ beforeNav
   return <Container maxWidth="lg">{children({ buttonBar: <ButtonBar beforeNavigate={beforeNavigate} /> })}</Container>
 }
 
-export function verifyAndSubmit<TFieldValues extends FieldValues = FieldValues>(
-  trigger: UseFormMethods<TFieldValues>["trigger"],
-  getValues: UseFormMethods<TFieldValues>["getValues"],
-  onValid: (data: UnpackNestedValue<TFieldValues>) => boolean | Promise<boolean>
-) {
-  return async (event?: React.BaseSyntheticEvent) => {
-    event?.preventDefault()
-    if (await trigger()) {
-      return await onValid(getValues())
-    } else {
-      return false
-    }
-  }
-}
-
 export function useHandleSubmit<TFieldValues extends FieldValues = FieldValues>(
   methods: UseFormMethods<TFieldValues>,
   onValid: (data: UnpackNestedValue<TFieldValues>) => boolean | Promise<boolean>
 ) {
-  return useMemo(() => {
-    return verifyAndSubmit(methods.trigger, methods.getValues, onValid)
-  }, [methods.trigger, methods.getValues, onValid])
+  const { handleSubmit } = methods
+  return useCallback(
+    async (event?: React.BaseSyntheticEvent) => {
+      let result = false
+      await handleSubmit(async values => {
+        result = await onValid(values)
+      })(event)
+      return result
+    },
+    [handleSubmit, onValid]
+  )
 }
 
 export default ApplicationStepModule
