06d90482120da516704ba70c8d41ea340ae56fbd
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-10-13 17:11:41 +0700

    remove: unnecessary provider

--- src/core/providers/index.provider.tsx
+++ src/core/providers/index.provider.tsx
@@ -2,15 +2,12 @@ import React from "react"
 import { AuthProvider } from "./auth.provider"
 import { GlobalProvider } from "./global.provider"
 import { ApplicationProvider } from "./application.provider"
-import { UserProvider } from "./user.provider"
 
 const AppProvider: React.FC = ({ children }) => {
   return (
     <GlobalProvider>
       <AuthProvider>
-        <UserProvider>
-          <ApplicationProvider>{children}</ApplicationProvider>
-        </UserProvider>
+        <ApplicationProvider>{children}</ApplicationProvider>
       </AuthProvider>
     </GlobalProvider>
   )
