1d81761a2ec9ed8e12ae12e5dfc1230df3d49ebe
zuikaru <pavat.tangmo@hotmail.com>
2020-10-25 01:31:35 +0700

    Fixed incorrect "มัธยมศึกษา"

--- src/core/constants/grades.constant.tsx
+++ src/core/constants/grades.constant.tsx
@@ -4,8 +4,8 @@ const gradesConstant: SelectModel = {
   name: "education",
   label: "ระดับชั้น",
   contents: [
-    { text: "ม.4", value: "มัญยมศึกษาปีที่ 4" },
-    { text: "ม.5", value: "มัญยมศึกษาปีที่ 5" },
+    { text: "ม.4", value: "มัธยมศึกษาปีที่ 4" },
+    { text: "ม.5", value: "มัธยมศึกษาปีที่ 5" },
     { text: "ปวช. ปี1", value: "ปวช. ปี1" },
     { text: "ปวช. ปี2", value: "ปวช. ปี2" }
   ]
