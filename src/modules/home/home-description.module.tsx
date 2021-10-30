d529bce5f405b153ea32aba869d1388eca3ce9f3
PKhing <apimrak@gmail.com>
2021-10-31 01:13:00 +0700

    💄 Edit home page description component

--- src/modules/home/home-description.module.tsx
+++ src/modules/home/home-description.module.tsx
@@ -1,44 +1,57 @@
 import React from "react"
-import { Typography, Grid } from "@material-ui/core"
+import { Typography } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { pxToRem } from "../../utils/conversion"
-import HomeContainer, { HomeContainerProps } from "./home-container.module"
+import { HomeContainerProps } from "./home-container.module"
+import { ITheme } from "../../styles/types"
+import tmp from "../../assets/images/description/tmp.png"
 
-const useStyle = makeStyles(theme => ({
-  title: {
-    fontFamily: "Raleway",
-    marginBottom: theme.spacing(4),
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "1.7rem"
+const useStyle = makeStyles((theme: ITheme) => ({
+  container: {
+    display: "flex",
+    color: theme.palette.gray[700],
+    padding: `${theme.spacing(9)}px min(${theme.spacing(9)}px, 5vw)`,
+    justifyContent: "center",
+    columnGap: "4vw",
+    alignItems: "center",
+    backgroundColor: "white",
+    [theme.breakpoints.down("lg")]: {
+      flexDirection: "column-reverse",
+      rowGap: `${theme.spacing(4)}px`
     }
   },
-  body: {
-    width: "90%",
-    marginLeft: "auto",
-    fontSize: "1.2rem",
-    fontWeight: 300,
-    lineHeight: pxToRem(33)
+  description: { lineHeight: pxToRem(42), marginTop: theme.spacing(4) },
+  descriptionContainer: {
+    maxWidth: "513px",
+    flex: 1,
+    [theme.breakpoints.down("lg")]: {
+      maxWidth: "80%",
+      textAlign: "center"
+    }
+  },
+  imageContainer: {
+    maxWidth: "498px",
+    flex: 1
   }
 }))
 
 const HomeLarngearDescription: React.FC<HomeContainerProps> = props => {
   const classes = useStyle()
   return (
-    <HomeContainer {...props}>
-      <Typography variant="h4" align="right" className={classes.title}>
-        What is Larngear&nbsp;Camp?
-      </Typography>
-      <Grid container justify="flex-end">
-        <Grid item sm={12} md={12} lg={9}>
-          <Typography align="right" className={classes.body} variant="body2">
-            ค่ายลานเกียร์เป็นค่ายที่จัดขึ้นในช่วงต้นของทุก ๆ ปี
-            ซึ่งจะเปิดโอกาสให้นักเรียนระดับชั้นมัธยมศึกษาตอนปลายทั่วประเทศที่สนใจศึกษาต่อในสาขาวิศวกรรมศาสตร์แต่ยังมีความไม่มั่นใจหรือมีข้อมูลไม่เพียงพอ
-            ได้เข้ามาสัมผัสการเรียนการสอนในวิชาปฏิบัติการจริง รวมไปถึงการทำกิจกรรมต่าง ๆ ที่สอดแทรกเนื้อหาเกี่ยวกับงานทางวิศวกรรม
-            เพื่อเป็นข้อมูลประกอบการตัดสินใจในการเลือกศึกษาต่อในระดับอุดมศึกษา
-          </Typography>
-        </Grid>
-      </Grid>
-    </HomeContainer>
+    <div className={classes.container}>
+      <div className={classes.descriptionContainer}>
+        <Typography variant="h1">ค่ายลานเกียร์คืออะไร?</Typography>
+        <Typography variant="subtitle2" className={classes.description}>
+          ค่ายลานเกียร์เป็นค่ายที่จัดขึ้นในเดือนมกราคมของทุกปีโดยเปิดโอกาสให้
+          นักเรียนระดับชั้นมัธยมศึกษาตอนปลายทั่วประเทศที่สนใจศึกษาต่อในสาขาวิศวกรรมศาสตร์แต่ยังไม่มั่นใจหรือมีข้อมูลไม่เพียงพอ ได้เข้ามาสัมผัสการ
+          เรียนการสอนในวิชาปฏิบัติการจริงรวมไปถึงการทำกิจกรรมต่าง ๆ ที่สอดแทรกเนื้อหาเกี่ยวกับงานทางวิศวกรรมเพื่อเป็นข้อมูลประกอบการ
+          ตัดสินใจในการเลือกศึกษาต่อในระดับอุดมศึกษา?
+        </Typography>
+      </div>
+      <div className={classes.imageContainer}>
+        <img src={tmp} alt="" />
+      </div>
+    </div>
   )
 }
 
