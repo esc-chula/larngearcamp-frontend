b74d0c645c97bd51b5978118d81dc3e9327bed27
Nonthapat Kaewamporn <tontan2545@gmail.com>
2021-11-01 22:38:01 +0700

    💄  styling to match new design

--- src/core/components/timeline.component.tsx
+++ src/core/components/timeline.component.tsx
@@ -1,13 +1,37 @@
 import React from "react"
-import { BoxProps } from "@material-ui/core"
+import { BoxProps, Typography } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { TimelineModel } from "../constants/timeline.constant"
-import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from "@material-ui/lab"
+import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator, TimelineOppositeContent } from "@material-ui/lab"
 
 const useStyle = makeStyles(theme => ({
+  timeline: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
   timelineContent: {
+    display: "flex",
+    flexDirection: "column",
+    position: "relative",
+    top: "-10px",
     textAlign: "left",
     paddingTop: "0"
+  },
+  eventTitle: {
+    fontSize: "30px",
+    width: "450px",
+    fontWeight: 500,
+    color: theme.palette.secondary.dark
+  },
+  durationTitle: {
+    ...theme.typography.subtitle1,
+    fontWeight: 300,
+    color: theme.palette.secondary.light
+  },
+  timelineSeparator: {
+    marginRight: "60px"
   },
   timelineDot: {
     margin: "0",
@@ -15,6 +39,14 @@ const useStyle = makeStyles(theme => ({
     aspectRatio: "1",
     padding: "0",
     backgroundColor: theme.palette.primary.main
+  },
+  timelineConnector: {
+    width: "4px",
+    height: "90px",
+    backgroundColor: theme.palette.primary.main
+  },
+  timelineOppositeContent: {
+    display: "none"
   }
 }))
 
@@ -30,15 +62,19 @@ export const TimelineDisplay: React.FC<TimelineProps> = props => {
   const classes = useStyle()
 
   return (
-    <Timeline>
+    <Timeline className={classes.timeline}>
       {label.map(({ duration, title }, index) => {
         return (
           <TimelineItem>
-            <TimelineSeparator>
+            <TimelineOppositeContent className={classes.timelineOppositeContent} />
+            <TimelineSeparator className={classes.timelineSeparator}>
               <TimelineDot className={classes.timelineDot} />
-              {index < label.length - 1 && <TimelineConnector />}
+              {index < label.length - 1 && <TimelineConnector className={classes.timelineConnector} />}
             </TimelineSeparator>
-            <TimelineContent className={classes.timelineContent}>{title}</TimelineContent>
+            <TimelineContent className={classes.timelineContent}>
+              <Typography className={classes.eventTitle}>{title}</Typography>
+              <Typography className={classes.durationTitle}>{duration}</Typography>
+            </TimelineContent>
           </TimelineItem>
         )
       })}
