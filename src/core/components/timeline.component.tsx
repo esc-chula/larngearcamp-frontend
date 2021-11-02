d1c7c61689e30ad52dcbb692e54f236995bcddba
Nonthapat Kaewamporn <tontan2545@gmail.com>
2021-11-02 16:14:23 +0700

    ⚡️ replaced momentjs with date-fns

--- src/core/components/timeline.component.tsx
+++ src/core/components/timeline.component.tsx
@@ -3,8 +3,8 @@ import { BoxProps, Typography } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { TimelineModel } from "../constants/timeline.constant"
 import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator, TimelineOppositeContent } from "@material-ui/lab"
-import moment, { Moment } from "moment"
-import "moment/locale/th"
+import { format, isWithinInterval, isFuture, isEqual, addYears } from "date-fns"
+import { th } from "date-fns/esm/locale"
 import { CSSProperties } from "@material-ui/styles"
 
 const useStyle = makeStyles(theme => ({
@@ -71,41 +71,44 @@ export const TimelineDisplay: React.FC<TimelineProps> = props => {
 
   const classes = useStyle()
 
-  const currentDate = moment()
+  const currentDate = new Date()
 
-  const isInactive = (endDate: Moment): CSSProperties | string => {
-    if (currentDate.isBefore(endDate)) return classes.inactive
+  const toChristianCalendar = (date: Date): Date => {
+    return addYears(date, -543)
+  }
+
+  const isInactive = (endDate: Date): CSSProperties | string => {
+    if (isFuture(toChristianCalendar(endDate))) return classes.inactive
     return ""
   }
 
-  const getDotStatus = (startDate: Moment, endDate: Moment): CSSProperties | string => {
-    if (currentDate.isBetween(startDate, endDate, undefined, "[]")) return classes.dotCurrentDate
+  const getDotStatus = (startDate: Date, endDate: Date): CSSProperties | string => {
+    if (isWithinInterval(currentDate, { start: toChristianCalendar(startDate), end: toChristianCalendar(endDate) })) return classes.dotCurrentDate
     return isInactive(endDate)
   }
 
-  const toMoment = (date: string): Moment => {
-    return moment(date, "DD MMMM YYYY").subtract(543, "year")
+  const formatDateTime = (date: Date): string => {
+    return format(date, "d MMMM yyyy", { locale: th })
   }
 
   return (
     <Timeline className={classes.timeline}>
       {label.map(({ duration, title }, index) => {
-        const startDate = toMoment(duration.start)
-        const endDate = toMoment(duration.end)
-
         return (
           <TimelineItem>
             <TimelineOppositeContent className={classes.timelineOppositeContent} />
             <TimelineSeparator className={classes.timelineSeparator}>
-              <TimelineDot className={`${classes.timelineDot} ${getDotStatus(startDate, endDate)}`} />
+              <TimelineDot className={`${classes.timelineDot} ${getDotStatus(duration.start, duration.end)}`} />
               {index < label.length - 1 && (
-                <TimelineConnector className={`${classes.timelineConnector} ${isInactive(toMoment(label[index + 1].duration.start))}`} />
+                <TimelineConnector className={`${classes.timelineConnector} ${isInactive(label[index + 1].duration.start)}`} />
               )}
             </TimelineSeparator>
             <TimelineContent className={classes.timelineContent}>
               <Typography className={classes.eventTitle}>{title}</Typography>
               <Typography className={classes.durationTitle}>
-                {startDate.isSame(endDate) ? duration.start : `${duration.start} - ${duration.end}`}
+                {isEqual(duration.start, duration.end)
+                  ? formatDateTime(duration.start)
+                  : `${formatDateTime(duration.start)} - ${formatDateTime(duration.end)}`}
               </Typography>
             </TimelineContent>
           </TimelineItem>
--- src/core/constants/timeline.constant.tsx
+++ src/core/constants/timeline.constant.tsx
@@ -1,51 +1,51 @@
 export interface TimelineModel {
   title: string
   duration: {
-    start: string
-    end: string
+    start: Date
+    end: Date
   }
 }
 
 const timelineConstant: Array<TimelineModel> = [
   {
     duration: {
-      start: "25 กันยายน 2563",
-      end: "20 ตุลาคม 2563"
+      start: new Date(2563, 9, 19),
+      end: new Date(2563, 10, 23)
     },
     title: "รับสมัคร"
   },
   {
     duration: {
-      start: "24 ตุลาคม 2563",
-      end: "24 ตุลาคม 2563"
+      start: new Date(2563, 10, 24),
+      end: new Date(2563, 10, 24)
     },
     title: "ประกาศผลผู้มีสิทธิ์สัมภาษณ์"
   },
   {
     duration: {
-      start: "31 ตุลาคม 2563",
-      end: "31 ตุลาคม 2563"
+      start: new Date(2563, 10, 31),
+      end: new Date(2563, 10, 31)
     },
     title: "สัมภาษณ์รอบกรุงเทพ"
   },
   {
     duration: {
-      start: "2 พฤศจิกายน 2563",
-      end: "8 พฤศจิกายน 2563"
+      start: new Date(2563, 11, 2),
+      end: new Date(2563, 11, 8)
     },
     title: "สัมภาษณ์รอบต่างจังหวัด"
   },
   {
     duration: {
-      start: "14 พฤศจิกายน 2563",
-      end: "14 พฤศจิกายน 2565"
+      start: new Date(2563, 11, 14),
+      end: new Date(2565, 11, 14)
     },
     title: "ประกาศรายชื่อผู้มีสิทธิ์เข้าร่วมค่าย"
   },
   {
     duration: {
-      start: "15 พฤศจิกายน 2565",
-      end: "19 ธันวาคม 2565"
+      start: new Date(2565, 12, 15),
+      end: new Date(2565, 12, 19)
     },
     title: "ยืนยันสิทธิ์การเข้าร่วมค่าย"
   }
