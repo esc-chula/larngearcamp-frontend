039beb3b77d2f4fc58c156730da67815e5aaedab
Nonthapat Kaewamporn <tontan2545@gmail.com>
2021-11-06 00:05:10 +0700

    ✨  facebook embed

--- src/modules/home/home-fbembed.module.tsx
+++ src/modules/home/home-fbembed.module.tsx
@@ -1,8 +1,34 @@
 import React from "react"
+import Iframe from "react-iframe"
+import { makeStyles } from "@material-ui/styles"
+import { Typography } from "@material-ui/core"
+
+const useStyle = makeStyles(theme => ({
+  facebookEmbed: {
+    margin: "auto",
+    border: "none",
+    overflow: "hidden"
+  }
+}))
 
 const HomeFacebookEmbed: React.FC = props => {
+  const classes = useStyle()
   return (
-    <div data-href="https://www.facebook.com/imdb" data-width="340" data-hide-cover="false" data-show-facepile="true" style={{ width: "500px" }} />
+    <React.Fragment>
+      <Typography align="center" variant="h2" style={{ margin: "50px 0" }}>
+        FOLLOW US
+      </Typography>
+      <Iframe
+        url="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FThailandIncubator%2F&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="500px"
+        height="600px"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        allowFullScreen={true}
+        frameBorder={0}
+        scrolling="no"
+        className={classes.facebookEmbed}
+      />
+    </React.Fragment>
   )
 }
 
