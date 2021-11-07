62948051fc440f30887e3d94851882b9e9a27f9c
PKhing <apimrak@gmail.com>
2021-11-07 21:56:48 +0700

    ⚡️ Improve description image preformance

--- src/modules/home/home-description-image.module.tsx
+++ src/modules/home/home-description-image.module.tsx
@@ -1,18 +1,25 @@
 import { Fade } from "@material-ui/core"
-import { makeStyles } from "@material-ui/styles"
+import { makeStyles } from "@material-ui/core/styles"
 import React, { useState, useCallback, useEffect } from "react"
 import descriptionConstant from "../../core/constants/description.constant"
 
-const useStyles = makeStyles(() => ({
-  image: { borderRadius: "10px", width: "100%" },
+const useStyles = makeStyles(theme => ({
+  image: {
+    borderRadius: "10px",
+    width: "min(497px, calc(100vw - 16px))",
+    height: "332px",
+    [theme.breakpoints.down("sm")]: {
+      height: `min( 332px, calc(${332 / 497} * (100vw - 16px)))`
+    }
+  },
   imageContainer: {
     maxWidth: "498px",
     flex: 1
   }
 }))
+
 const HomeDescriptionImage = () => {
   const classes = useStyles()
-
   const [index, setIndex] = useState(0)
   const changeImage = useCallback(() => {
     setIndex(index => (index + 1) % descriptionConstant.length)
@@ -31,17 +38,16 @@ const HomeDescriptionImage = () => {
     }
   }, [changeImage])
 
+  useEffect(() => {
+    setDisplay(true)
+  }, [index])
+
   return (
     <Fade in={display} timeout={500}>
-      <div className={classes.imageContainer}>
-        <img
-          src={descriptionConstant[index].src}
-          alt=""
-          className={classes.image}
-          onLoad={() => {
-            setDisplay(true)
-          }}
-        />
+      <div>
+        {descriptionConstant.map(({ src }, idx) => {
+          return <img src={src} key={idx} alt="descriptionImage" style={{ display: index === idx ? "block" : "none" }} className={classes.image} />
+        })}
       </div>
     </Fade>
   )
