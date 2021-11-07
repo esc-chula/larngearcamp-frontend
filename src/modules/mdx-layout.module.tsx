c02de7c050d74c11e3c7d0d0f0089412a8baa259
Saenyakorn <saneyakorn@gmail.com>
2021-11-07 17:42:46 +0700

    💄 add card for faq

--- src/modules/mdx-layout.module.tsx
+++ src/modules/mdx-layout.module.tsx
@@ -26,6 +26,22 @@ const A = styled("a")(({ theme }) => ({
   }
 }))
 
+const Card = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  borderRadius: theme.shape.borderRadius,
+  boxShadow: theme.shadows[2],
+  padding: theme.spacing(5),
+  margin: theme.spacing(2, 0),
+  [theme.breakpoints.down("sm")]: {
+    padding: theme.spacing(2)
+  }
+}))
+
+const CardHeader = styled(Typography)(({ theme }) => ({
+  color: theme.palette.primary.main,
+  marginBottom: theme.spacing(2)
+}))
+
 const H1: React.FC = props => <Typography variant="h2" component="h1" color="primary" style={{ textAlign: "center" }} {...props} />
 
 const H2: React.FC = props => <Typography variant="subtitle1" component="h2" {...props} />
@@ -35,7 +51,9 @@ export const MDXLayout: React.FC<Omit<MDXProviderProps, "components">> = props =
     wrapper: wrapper,
     h1: H1,
     h2: H2,
-    a: props => <A className="no-underline" {...props} />
+    a: props => <A className="no-underline" {...props} />,
+    Card: Card,
+    CardHeader: props => <CardHeader variant="subtitle1" {...props} />
   }
   return <MDXProvider {...props} components={components} />
 }
