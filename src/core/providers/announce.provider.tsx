474d6e008d681a4124c542043ca3974d3becc0e4
miello <partearth1212@gmail.com>
2021-11-12 18:54:56 +0700

    🔥 Remove unused components

--- src/core/providers/announce.provider.tsx
+++ src/core/providers/announce.provider.tsx
@@ -11,9 +11,9 @@ export const AnnounceContext = createContext({} as AnnounceConstruct)
 export const useAnnounceContext = () => useContext(AnnounceContext)
 
 export const AnnounceProvider: React.FC = ({ ...other }) => {
-  const registerCloseDate = new Date("December 8, 2021 23:59:59 GMT+07:00")
+  const registerCloseDate = new Date("December 9, 2021 00:00:00 GMT+07:00")
   const applicationStartDate = new Date("November 15, 2021 10:00:00 GMT+07:00")
-  const isLate = new Date() > registerCloseDate
+  const isLate = new Date() >= registerCloseDate
   const isEarly = new Date() < applicationStartDate
   const isApplicable = !isEarly && !isLate
 
