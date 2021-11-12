5f177a774f0401751627682a85d913d4e6c4d68b
miello <partearth1212@gmail.com>
2021-11-12 18:05:36 +0700

    💄 Hide register button when not in range

--- src/core/providers/announce.provider.tsx
+++ src/core/providers/announce.provider.tsx
@@ -3,6 +3,7 @@ import React, { createContext, useContext } from "react"
 interface AnnounceConstruct {
   announceDate: Date
   applicationStartDate: Date
+  isApplicable: boolean
 }
 
 export const AnnounceContext = createContext({} as AnnounceConstruct)
@@ -10,8 +11,10 @@ export const AnnounceContext = createContext({} as AnnounceConstruct)
 export const useAnnounceContext = () => useContext(AnnounceContext)
 
 export const AnnounceProvider: React.FC = ({ ...other }) => {
-  const announceDate = new Date("December 8, 2021 18:00:00 GMT+07:00")
+  const announceDate = new Date("December 8, 2021 23:59:59 GMT+07:00")
   const applicationStartDate = new Date("November 15, 2021 10:00:00 GMT+07:00")
-  const value = { announceDate, applicationStartDate }
+  const isApplicable = new Date() >= applicationStartDate && new Date() <= announceDate
+
+  const value = { announceDate, applicationStartDate, isApplicable }
   return <AnnounceContext.Provider value={value} {...other} />
 }
