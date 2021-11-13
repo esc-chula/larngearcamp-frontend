3773ba25dd8c23ecc5139716e08774113cb26882
Saenyakorn <saneyakorn@gmail.com>
2021-11-13 13:22:58 +0700

    fix build failed

--- src/modules/mdx-layout.module.tsx
+++ src/modules/mdx-layout.module.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from "react"
-import Particles, { ISourceOptions } from "react-tsparticles"
+// import Particles, { ISourceOptions } from "react-tsparticles"
 import { MDXProvider, MDXProviderComponents, MDXProviderProps } from "@mdx-js/react"
 import { styled } from "@material-ui/styles"
 import { makeStyles, Typography, useTheme } from "@material-ui/core"
@@ -64,117 +64,117 @@ export const MDXLayout: React.FC<Omit<MDXProviderProps, "components">> = props =
     Card: Card,
     CardHeader: props => <CardHeader variant="subtitle1" {...props} />
   }
-  const classes = useStyles()
-  const theme = useTheme()
-  const options: ISourceOptions = useMemo(
-    () => ({
-      particles: {
-        number: {
-          value: 80,
-          density: {
-            enable: true,
-            value_area: 800
-          }
-        },
-        color: {
-          value: [theme.palette.primary.main]
-        },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#b6b2b2"
-          }
-        },
-        opacity: {
-          value: 0.5211089197812949,
-          random: false,
-          anim: {
-            enable: true,
-            speed: 1,
-            opacity_min: 0.1,
-            sync: false
-          }
-        },
-        size: {
-          value: 2,
-          random: true,
-          anim: {
-            enable: true,
-            speed: 12.181158184520175,
-            size_min: 0.1,
-            sync: true
-          }
-        },
-        line_linked: {
-          enable: true,
-          distance: 150,
-          color: "#c8c8c8",
-          opacity: 0.4,
-          width: 1
-        },
-        move: {
-          enable: true,
-          speed: 1,
-          direction: "none",
-          random: true,
-          straight: false,
-          out_mode: "bounce",
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200
-          }
-        }
-      },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: {
-            enable: false,
-            mode: "repulse"
-          },
-          onclick: {
-            enable: false,
-            mode: "push"
-          },
-          resize: true
-        },
-        modes: {
-          grab: {
-            distance: 400,
-            line_linked: {
-              opacity: 1
-            }
-          },
-          bubble: {
-            distance: 400,
-            size: 40,
-            duration: 2,
-            opacity: 8
-            // speed: 3,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4
-          },
-          push: {
-            particles_nb: 4
-          },
-          remove: {
-            particles_nb: 2
-          }
-        }
-      },
-      retina_detect: true
-    }),
-    [theme]
-  )
+  // const classes = useStyles()
+  // const theme = useTheme()
+  // const options: ISourceOptions = useMemo(
+  //   () => ({
+  //     particles: {
+  //       number: {
+  //         value: 80,
+  //         density: {
+  //           enable: true,
+  //           value_area: 800
+  //         }
+  //       },
+  //       color: {
+  //         value: [theme.palette.primary.main]
+  //       },
+  //       shape: {
+  //         type: "circle",
+  //         stroke: {
+  //           width: 0,
+  //           color: "#b6b2b2"
+  //         }
+  //       },
+  //       opacity: {
+  //         value: 0.5211089197812949,
+  //         random: false,
+  //         anim: {
+  //           enable: true,
+  //           speed: 1,
+  //           opacity_min: 0.1,
+  //           sync: false
+  //         }
+  //       },
+  //       size: {
+  //         value: 2,
+  //         random: true,
+  //         anim: {
+  //           enable: true,
+  //           speed: 12.181158184520175,
+  //           size_min: 0.1,
+  //           sync: true
+  //         }
+  //       },
+  //       line_linked: {
+  //         enable: true,
+  //         distance: 150,
+  //         color: "#c8c8c8",
+  //         opacity: 0.4,
+  //         width: 1
+  //       },
+  //       move: {
+  //         enable: true,
+  //         speed: 1,
+  //         direction: "none",
+  //         random: true,
+  //         straight: false,
+  //         out_mode: "bounce",
+  //         bounce: false,
+  //         attract: {
+  //           enable: false,
+  //           rotateX: 600,
+  //           rotateY: 1200
+  //         }
+  //       }
+  //     },
+  //     interactivity: {
+  //       detect_on: "canvas",
+  //       events: {
+  //         onhover: {
+  //           enable: false,
+  //           mode: "repulse"
+  //         },
+  //         onclick: {
+  //           enable: false,
+  //           mode: "push"
+  //         },
+  //         resize: true
+  //       },
+  //       modes: {
+  //         grab: {
+  //           distance: 400,
+  //           line_linked: {
+  //             opacity: 1
+  //           }
+  //         },
+  //         bubble: {
+  //           distance: 400,
+  //           size: 40,
+  //           duration: 2,
+  //           opacity: 8
+  //           // speed: 3,
+  //         },
+  //         repulse: {
+  //           distance: 200,
+  //           duration: 0.4
+  //         },
+  //         push: {
+  //           particles_nb: 4
+  //         },
+  //         remove: {
+  //           particles_nb: 2
+  //         }
+  //       }
+  //     },
+  //     retina_detect: true
+  //   }),
+  //   [theme]
+  // )
   return (
     <>
       <MDXProvider {...props} components={components} />
-      <Particles id="tsparticles" options={options} className={classes.tsparticles} />
+      {/* <Particles id="tsparticles" options={options} className={classes.tsparticles} /> */}
     </>
   )
 }
