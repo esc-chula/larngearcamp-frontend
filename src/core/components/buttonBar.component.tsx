b80f1012c13b75b620a9db61b5c70bb3ac6f7957
Saenyakorn <saneyakorn@gmail.com>
2021-11-14 19:39:31 +0700

    fix application button styles

--- src/core/components/buttonBar.component.tsx
+++ src/core/components/buttonBar.component.tsx
@@ -1,7 +1,6 @@
 import React, { useMemo } from "react"
-import { useRouteMatch, Link, useHistory } from "react-router-dom"
+import { useRouteMatch, useHistory } from "react-router-dom"
 import { Grid, Button } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
 import { useLoadingStatus } from "./loading.component"
 import { useApplicationStateContext } from "../providers/applicationState.provider"
 
@@ -9,31 +8,11 @@ export type ButtonBarProps = {
   beforeNavigate?: () => boolean | Promise<boolean>
 }
 
-const useStyles = makeStyles(theme => ({
-  buttonSuccess: {
-    marginTop: theme.spacing(2),
-    color: "white",
-    background: theme.palette.success.main,
-    "&:hover": {
-      background: theme.palette.success.dark
-    }
-  },
-  buttonWarning: {
-    marginTop: theme.spacing(2),
-    color: "white",
-    background: theme.palette.warning.main,
-    "&:hover": {
-      background: theme.palette.warning.dark
-    }
-  }
-}))
-
 const ButtonBar: React.FC<ButtonBarProps> = ({ beforeNavigate }) => {
   const step = parseInt(useRouteMatch<{ step: string }>().params.step)
   const history = useHistory()
   const previousPage = `/application/step/${step - 1}`
   const nextPage = `/application/step/${step + 1}`
-  const classes = useStyles()
   const setLoading = useLoadingStatus()
   const { application } = useApplicationStateContext()
 
@@ -49,11 +28,21 @@ const ButtonBar: React.FC<ButtonBarProps> = ({ beforeNavigate }) => {
     }
   }, [beforeNavigate, setLoading])
 
+  const getConfirmation = () => {
+    return window.confirm("ระบบยังไม่บันทึกข้อมูล คุณต้องการออกจากหน้านี้ใช่หรือไม่")
+  }
+
+  const handlePrevious = () => {
+    if (getConfirmation()) {
+      history.push(previousPage)
+    }
+  }
+
   const handleNext = async (event: React.MouseEvent<HTMLButtonElement>) => {
     const whiteList = ["/application/step/1", "/application"]
     let confirmation
     if (whiteList.indexOf(history.location.pathname) >= 0) confirmation = true
-    else confirmation = window.confirm("คุณต้องการออกจากหน้านี้ใช่หรือไม่")
+    else confirmation = getConfirmation()
 
     if (confirmation && wrappedBeforeNavigate) {
       event.preventDefault()
@@ -67,17 +56,15 @@ const ButtonBar: React.FC<ButtonBarProps> = ({ beforeNavigate }) => {
 
   return (
     <Grid container spacing={2}>
-      {showBothButtons ? (
+      {showBothButtons && (
         <Grid xs={12} sm={6} item>
-          <Link className="no-underline" to={previousPage}>
-            <Button variant="contained" className={classes.buttonWarning} fullWidth>
-              {step === 6 ? "กลับไปแก้ไขหน้าที่แล้ว" : "ย้อนกลับ"}
-            </Button>
-          </Link>
+          <Button variant="outlined" color="primary" fullWidth onClick={handlePrevious}>
+            {step === 6 ? "กลับไปแก้ไขหน้าที่แล้ว" : "ย้อนกลับ"}
+          </Button>
         </Grid>
-      ) : null}
+      )}
       <Grid xs={12} sm={showBothButtons ? 6 : 12} item>
-        <Button variant="contained" className={classes.buttonSuccess} fullWidth type="submit" onClick={handleNext}>
+        <Button variant="contained" color="primary" fullWidth type="submit" onClick={handleNext}>
           {application.state === "SUBMITTED" ? "ยืนยันการแก้ไข" : step === 6 ? "ยืนยันการสมัคร" : "ไปขั้นตอนถัดไป"}
         </Button>
       </Grid>
