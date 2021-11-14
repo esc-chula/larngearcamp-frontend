a674e1fdf4b5aa834ae0a26ed7b6c60256df1dec
Saenyakorn <saneyakorn@gmail.com>
2021-11-14 19:39:49 +0700

    change proxy to dev environment

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://larngear.in.th/",
+      target: "https://larngear.saenyakorn.dev",
       changeOrigin: true
     })
   )
