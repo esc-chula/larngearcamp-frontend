f90cd871b2e1ad95c27d76bcf45d1b7c506f983c
PongDev <pongwiwat.dev@gmail.com>
2022-08-26 18:41:42 +0700

    Change Number from Larngear 21 -> 22

--- src/core/constants/uploadFile.constant.tsx
+++ src/core/constants/uploadFile.constant.tsx
@@ -10,7 +10,7 @@ const uploadFileConstant: Array<UploadFileModel> = [
   },
   {
     name: "parentalConsent",
-    body1: "หนังสือรับรองจากผู้ปกครองเพื่อเข้าร่วมค่ายลานเกียร์ครั้งที่ 21",
+    body1: "หนังสือรับรองจากผู้ปกครองเพื่อเข้าร่วมค่ายลานเกียร์ครั้งที่ 22",
     body2: "ไฟล์ประเภท .pdf / .jpg / .png ขนาดไม่เกิน 4 MB",
     accept: "application/pdf, image/jpeg, image/png",
     size: 4000000
