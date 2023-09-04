77f9c81ce7e5ad744613e0d3dbf92b24877f698d
นภันต์ โชติช่วงนภา <naphanccnp@gmail.com>
2023-09-05 00:41:24 +0700

    remove landing page announcement

--- src/core/constants/announcements.constant.tsx
+++ src/core/constants/announcements.constant.tsx
@@ -8,24 +8,24 @@ interface AnnouncementConstant {
 }
 
 export const announcementConstant: AnnouncementConstant[] = [
-  {
-    severity: "error",
-    variant: "standard",
-    content: (
-      <>
-        ขอให้น้องผู้มีสิทธิ์เข้าค่ายทุกคน ดาวน์โหลดไฟล์{" "}
-        <a href="/assets/file/หนังสือรับทราบและยินยอมปฏิบัติตามมาตรการ.pdf" target="_blank" rel="noopener noreferrer" className="no-underline">
-          หนังสือรับทราบและยินยอมปฏิบัติตามมาตรการการป้องกันโรคติดเชื้อไวรัสโคโรนา 2019
-        </a>{" "}
-        แล้วนำส่งใบตอบรับที่{" "}
-        <a href="https://forms.gle/cDWLrLLq8iqRngkQ7" target="_blank" rel="noopener noreferrer" className="no-underline">
-          Google Form
-        </a>{" "}
-        ภายในวันที่ 5 มกราคม 2566 ดูรายละเอียดเพิ่มเติมได้ที่เพจ{" "}
-        <a href="https://www.facebook.com/LARNGEARCAMP" target="_blank" rel="noopener noreferrer" className="no-underline">
-          Larngear Camp
-        </a>
-      </>
-    )
-  }
+  // {
+  //   severity: "error",
+  //   variant: "standard",
+  //   content: (
+  //     <>
+  //       ขอให้น้องผู้มีสิทธิ์เข้าค่ายทุกคน ดาวน์โหลดไฟล์{" "}
+  //       <a href="/assets/file/หนังสือรับทราบและยินยอมปฏิบัติตามมาตรการ.pdf" target="_blank" rel="noopener noreferrer" className="no-underline">
+  //         หนังสือรับทราบและยินยอมปฏิบัติตามมาตรการการป้องกันโรคติดเชื้อไวรัสโคโรนา 2019
+  //       </a>{" "}
+  //       แล้วนำส่งใบตอบรับที่{" "}
+  //       <a href="https://forms.gle/cDWLrLLq8iqRngkQ7" target="_blank" rel="noopener noreferrer" className="no-underline">
+  //         Google Form
+  //       </a>{" "}
+  //       ภายในวันที่ 5 มกราคม 2566 ดูรายละเอียดเพิ่มเติมได้ที่เพจ{" "}
+  //       <a href="https://www.facebook.com/LARNGEARCAMP" target="_blank" rel="noopener noreferrer" className="no-underline">
+  //         Larngear Camp
+  //       </a>
+  //     </>
+  //   )
+  // }
 ]
