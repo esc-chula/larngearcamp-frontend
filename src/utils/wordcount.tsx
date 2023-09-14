24b0e3603c9ad435e7e8c5e0a0671758596e14d4
LittleNa1000 <naphanccnp@gmail.com>
2023-09-14 17:30:36 +0700

    test wordcut-ts

--- src/utils/wordcount.tsx
+++ src/utils/wordcount.tsx
@@ -1,9 +1,19 @@
-export const wordcut = require("wordcut")
+import { Wordcut } from "wordcut-ts"
+import { WordcutCore } from "wordcut-ts/lib/wordcut_core"
 
-// wordcut.init()
+let wordcut: WordcutCore
+try {
+  wordcut = new Wordcut()
+  wordcut.initNoDict()
+} catch (e) {
+  console.log("wordcut init err", e)
+}
 
 export const wordCount = (text: string) => {
-  wordcut.init()
+  if (!wordcut) {
+    console.log("wordcut not init")
+    return text.length
+  }
   const words = (wordcut.cut(text.trim()) as string).split("|").filter((word: string) => word.trim().length > 1)
   // const words = text
   return words.length
