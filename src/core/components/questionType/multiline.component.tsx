5ac53a7109b71d4c5bd2b5b08f4652a1cf2e1482
LittleNa1000 <naphanccnp@gmail.com>
2023-09-14 19:33:42 +0700

    Revert "add wordcount"
    This reverts commit 5a4612011c314512407c7b8750fac835ec562461.

--- src/core/components/questionType/multiline.component.tsx
+++ src/core/components/questionType/multiline.component.tsx
@@ -3,8 +3,6 @@ import { StandardTextFieldProps, TextField } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { useFormContext } from "react-hook-form"
 import { resolve } from "../../../utils/modify"
-import { log } from "console"
-import { wordCount } from "../../../utils/wordcount"
 
 interface MultiLineTypeProps {
   name: string
@@ -24,27 +22,19 @@ const MultilineTypeComponent: React.FC<StandardTextFieldProps & MultiLineTypePro
   const classes = useStyles()
   const { register, errors } = useFormContext()
   const selfError = resolve(name, errors)
-  let count = 0
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    count = wordCount(event.target.value)
-    console.log(count)
-  }
+
   return (
-    <>
-      <TextField
-        variant="outlined"
-        size="small"
-        multiline
-        inputRef={register}
-        className={classes.multiline}
-        name={name}
-        error={!!selfError}
-        helperText={selfError?.message}
-        {...other}
-        onChange={handleChange}
-      />
-      <span>Count: {count}</span>
-    </>
+    <TextField
+      variant="outlined"
+      size="small"
+      multiline
+      inputRef={register}
+      className={classes.multiline}
+      name={name}
+      error={!!selfError}
+      helperText={selfError?.message}
+      {...other}
+    />
   )
 }
 
--- src/utils/wordcount.tsx
+++
@@ -1,8 +0,0 @@
-const wordcut = require("wordcut")
-
-wordcut.init()
-
-export const wordCount = (text: string) => {
-  const words = (wordcut.cut(text.trim()) as string).split("|").filter((word: string) => word.trim().length > 1)
-  return words.length
-}
