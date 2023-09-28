153e45a8d099a88e6371ee785e8f362086386140
LittleNa1000 <naphanccnp@gmail.com>
2023-09-29 01:43:49 +0700

    add announcement

--- src/core/constants/announcements.constant.tsx
+++ src/core/constants/announcements.constant.tsx
@@ -8,24 +8,14 @@ interface AnnouncementConstant {
 }
 
 export const announcementConstant: AnnouncementConstant[] = [
-  // {
-  //   severity: "error",
-  //   variant: "standard",
-  //   content: (
-  //     <>
-  //       ขอให้น้องผู้มีสิทธิ์เข้าค่ายทุกคน ดาวน์โหลดไฟล์{" "}
-  //       <a href="/assets/file/หนังสือรับทราบและยินยอมปฏิบัติตามมาตรการ.pdf" target="_blank" rel="noopener noreferrer" className="no-underline">
-  //         หนังสือรับทราบและยินยอมปฏิบัติตามมาตรการการป้องกันโรคติดเชื้อไวรัสโคโรนา 2019
-  //       </a>{" "}
-  //       แล้วนำส่งใบตอบรับที่{" "}
-  //       <a href="https://forms.gle/cDWLrLLq8iqRngkQ7" target="_blank" rel="noopener noreferrer" className="no-underline">
-  //         Google Form
-  //       </a>{" "}
-  //       ภายในวันที่ 5 มกราคม 2566 ดูรายละเอียดเพิ่มเติมได้ที่เพจ{" "}
-  //       <a href="https://www.facebook.com/LARNGEARCAMP" target="_blank" rel="noopener noreferrer" className="no-underline">
-  //         Larngear Camp
-  //       </a>
-  //     </>
-  //   )
-  // }
+  {
+    severity: "warning",
+    variant: "standard",
+    content: (
+      <>
+        สำหรับน้อง ๆ คนใดที่ในขณะนี้ยังมีเอกสารการสมัครที่ไม่พร้อมส่ง ให้ส่งไฟล์เปล่าของอะไรก็ได้เข้ามาก่อน และ เมื่อสมัครเรียบร้อย
+        จะมีเวลาให้แก้เอกสารอีกครั้ง
+      </>
+    )
+  }
 ]
