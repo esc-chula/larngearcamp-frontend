6aef2c5fa52cb92a562c414c578700852da43c59
JomnoiZ <kamanun.m@gmail.com>
2024-08-29 19:07:51 +0700

    change instagram embedded post url

--- src/modules/home/home-fbembed.module.tsx
+++ src/modules/home/home-fbembed.module.tsx
@@ -61,7 +61,7 @@ const HomeFacebookEmbed: React.FC = props => {
           className={classes.facebookEmbed}
         />
         <Iframe
-          url={`https://www.instagram.com/p/C_NoAsuzSGH/?img_index=2/embed`}
+          url={`https://www.instagram.com/p/C_NoAsuzSGH/embed?img_index=2`}
           width={`${width}px`}
           height={`${IGHeight}px`}
           allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
