719bee89889eee5788ff72735d9cfc6966aab847
JomnoiZ <kamanun.m@gmail.com>
2024-08-29 18:36:12 +0700

    edit contact list

--- src/core/constants/contact.constant.tsx
+++ src/core/constants/contact.constant.tsx
@@ -8,7 +8,7 @@ const contactConstant: Array<ContactModel> = [
   { name: "พี่เทม ( P’Thems )", lineId: "LineID : thems36970" },
   { name: "พี่แซนด์ ( P’Sand )", lineId: "LineID : sand17226sand" },
   { name: "พี่พลอย ( P’Ploy )", lineId: "LineID : ployppf" },
-  { name: "พี่เมอร์ก ( P’Merk )", lineId: "LineID : 0971548843" }
+  { name: "พี่เมอร์ก ( P’Maersk )", lineId: "LineID : 097-154-8843 (เบอร์โทรศัพท์)" }
 ]
 
 export default contactConstant
